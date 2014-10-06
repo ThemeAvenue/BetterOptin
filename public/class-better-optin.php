21d75c31529f2b82b988ed5575dd701222249b39
Julien Liabeuf <julien@liabeuf.fr>
2014-10-06 09:08:08 +0700

    Don't use variables when loading translation files

--- public/class-better-optin.php
+++ public/class-better-optin.php
@@ -216,11 +216,9 @@ class Better_Optin {
 	 */
 	public function load_plugin_textdomain() {
 
-		$domain = $this->plugin_slug;
-		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+		$locale = apply_filters( 'plugin_locale', get_locale(), 'wpbo' );
 
-		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
-		load_plugin_textdomain( $domain, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . '/languages/' );
+		load_plugin_textdomain( 'wpbo', FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . '/languages/' );
 
 	}
 
