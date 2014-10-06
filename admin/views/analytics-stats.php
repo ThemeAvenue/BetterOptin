ThemeAvenue/BetterOptin

commit c4f17174ac4e12b62b6df93d0bab78165e1c38d5

Correct wrong calculation for the conversion rate

admin/views/analytics-stats.php
@@ -137,10 +137,11 @@ switch( $period ) {
 /* Set the period */
 $query['period'] = $timeframe;
 
-// print_r( $query );
-
 /* Get the datas */
 $datas = wpbo_get_datas( $query, 'OBJECT' );
+
+/* Get the grand total */
+$grand_total = count( $datas );
 ?>
 <div class="postbox">
 
@@ -160,12 +161,13 @@ $datas = wpbo_get_datas( $query, 'OBJECT' );
 			</thead>
 			<tbody>
 				<?php
-				if( is_array( $datas ) ) {
+				if ( is_array( $datas ) ) {
 
-					foreach( $datas as $key => $data ) {
+					foreach ( $datas as $key => $data ) {
 
-						if( in_array( $data->popup_id, $seen ) )
+						if ( in_array( $data->popup_id, $seen ) ) {
 							continue;
+						}
 
 						/* Mark this popup as parsed */
 						array_push( $seen, $data->popup_id );
@@ -173,7 +175,7 @@ $datas = wpbo_get_datas( $query, 'OBJECT' );
 						$popup        = get_post( $data->popup_id );
 						$impressions  = wpbo_get_datas( array( 'popup_id' => $data->popup_id, 'data_type' => 'impression', 'limit' => -1, 'period' => $timeframe ), 'ARRAY_A' );
 						$conversions  = wpbo_get_datas( array( 'popup_id' => $data->popup_id, 'data_type' => 'conversion', 'limit' => -1, 'period' => $timeframe ), 'ARRAY_A' );
-						$rate         = ( 0 === count( $conversions ) || 0 === count( $impressions ) ) ? 0 : ( 100 * count( $conversions ) ) / count( $impressions );
+						$rate         = ( 0 === count( $conversions ) || 0 === count( $impressions ) ) ? 0 : ( 100 * count( $conversions ) ) / $grand_total;
 						$status       = 'publish' == $popup->post_status ? __( 'Active', 'wpbo' ) : __( 'Inactive', 'wpbo' );
 						$status_class = 'publish' == $popup->post_status ? 'wpbo-stats-active' : 'wpbo-stats-inactive';
 

admin/views/analytics.php
@@ -114,7 +114,7 @@ $period = isset( $_GET['period'] ) ? $_GET['period'] : 'today';
 
 						<h3><span><?php printf( __( '%s\'s Stats Breakdown', 'wpbo' ), ucwords( str_replace( '_', ' ', $period ) ) ); ?></span></h3>
 						<?php
-						$total_rate = ( 0 === $total_conversions || 0 === $total_impressions ) ? 0 : ( 100 * $total_conversions ) / $total_impressions;
+						$total_rate = ( 0 === $total_conversions || 0 === $total_impressions ) ? 0 : ( 100 * $total_conversions ) / $grand_total;
 						$total_rate = number_format( $total_rate, 2 );
 						?>
 						<div class="inside">

commit a48136fb5f7622094f1600d2fdf57d9ca1300039

Fix conversion rate calculations errors (caused by a misunderstanding in commit c4f17174ac4e12b62b6df93d0bab78165e1c38d5)

admin/views/analytics-stats.php
@@ -137,11 +137,10 @@ switch( $period ) {
 /* Set the period */
 $query['period'] = $timeframe;
 
+// print_r( $query );
+
 /* Get the datas */
 $datas = wpbo_get_datas( $query, 'OBJECT' );
-
-/* Get the grand total */
-$grand_total = count( $datas );
 ?>
 <div class="postbox">
 
@@ -161,13 +160,12 @@ $grand_total = count( $datas );
 			</thead>
 			<tbody>
 				<?php
-				if ( is_array( $datas ) ) {
+				if( is_array( $datas ) ) {
 
-					foreach ( $datas as $key => $data ) {
+					foreach( $datas as $key => $data ) {
 
-						if ( in_array( $data->popup_id, $seen ) ) {
+						if( in_array( $data->popup_id, $seen ) )
 							continue;
-						}
 
 						/* Mark this popup as parsed */
 						array_push( $seen, $data->popup_id );
@@ -175,7 +173,7 @@ $grand_total = count( $datas );
 						$popup        = get_post( $data->popup_id );
 						$impressions  = wpbo_get_datas( array( 'popup_id' => $data->popup_id, 'data_type' => 'impression', 'limit' => -1, 'period' => $timeframe ), 'ARRAY_A' );
 						$conversions  = wpbo_get_datas( array( 'popup_id' => $data->popup_id, 'data_type' => 'conversion', 'limit' => -1, 'period' => $timeframe ), 'ARRAY_A' );
-						$rate         = ( 0 === count( $conversions ) || 0 === $grand_total ) ? 0 : ( 100 * count( $conversions ) ) / $grand_total;
+						$rate         = ( 0 === count( $conversions ) || 0 === count( $impressions ) ) ? 0 : ( 100 * count( $conversions ) ) / count( $impressions );
 						$status       = 'publish' == $popup->post_status ? __( 'Active', 'wpbo' ) : __( 'Inactive', 'wpbo' );
 						$status_class = 'publish' == $popup->post_status ? 'wpbo-stats-active' : 'wpbo-stats-inactive';
 

admin/views/analytics.php
@@ -114,7 +114,7 @@ $period = isset( $_GET['period'] ) ? $_GET['period'] : 'today';
 
 						<h3><span><?php printf( __( '%s\'s Stats Breakdown', 'wpbo' ), ucwords( str_replace( '_', ' ', $period ) ) ); ?></span></h3>
 						<?php
-						$total_rate = ( 0 === $total_conversions || 0 === $grand_total ) ? 0 : ( 100 * $total_conversions ) / $grand_total;
+						$total_rate = ( 0 === $total_conversions || 0 === $total_impressions ) ? 0 : ( 100 * $total_conversions ) / $total_impressions;
 						$total_rate = number_format( $total_rate, 2 );
 						?>
 						<div class="inside">

public/includes/class-analytics.php
@@ -518,19 +518,26 @@ function wpbo_today_conversion( $decimals = 2, $dec_point = '.', $thousands_sep 
 
 	/* Set the count vars. */
 	$total       = count( $datas );
+	$impressions = 0;
 	$conversions = 0;
 
 	/* Check the number of conversions. */
 	foreach ( $datas as $data ) {
 
+		/* Increment conversions */
 		if ( 'conversion' == $data->data_type ) {
 			++$conversions;
+		}
+
+		/* Increment impressions */
+		if ( 'impression' == $data->data_type ) {
+			++$impressions;
 		}
 
 	}
 
 	/* Get the converison rate. */
-	$rate = ( $conversions * 100 ) / $total;
+	$rate = ( 0 === $conversions || 0 === $impressions ) ? 0 : ( $conversions * 100 ) / $impressions;
 
 	return number_format( $rate, $decimals, $dec_point, $thousands_sep );
 