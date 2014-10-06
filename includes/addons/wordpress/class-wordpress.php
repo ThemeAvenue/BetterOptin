bf253d127c1bfdff37dfe8a69a5791d9be9c5b21
Julien Liabeuf <julien@liabeuf.fr>
2014-10-06 13:11:15 +0700

    Avoid undefined variable error and load the parent class to get post and popup IDs

--- includes/addons/wordpress/class-wordpress.php
+++ includes/addons/wordpress/class-wordpress.php
@@ -4,6 +4,12 @@ class WPBO_WordPress extends WPBO_Submit {
 	public function __construct( $main = false ) {
 
 		global $post;
+
+		/**
+		 * Load the parent's constructor in order to correctly get
+		 * the popup and post IDs.
+		 */
+		parent::__construct();
 		
 		/**
 		 * Set vars
@@ -223,8 +229,8 @@ class WPBO_WordPress extends WPBO_Submit {
 		$args = array(
 			'user_email'   => sanitize_email( $_POST['wpbo_email'] ),
 			'user_login'   => sanitize_email( $_POST['wpbo_email'] ),
-			'first_name'   => $extra['first_name'],
-			'display_name' => $extra['first_name'],
+			'first_name'   => isset( $extra['first_name'] ) ? $extra['first_name'] : $_POST['wpbo_email'],
+			'display_name' => isset( $extra['first_name'] ) ? $extra['first_name'] : $_POST['wpbo_email'],
 			'user_pass'    => md5( $this->password ),
 			'role'         => $this->role
 		);
