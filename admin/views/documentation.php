d82f4156712de7c09f685975308c361cb96c997b
Julien Vernet <julien@vernet.me>
2015-10-06 20:00:11 +0700

    Adjust styling accordingly - #28

--- admin/views/documentation.php
+++ admin/views/documentation.php
@@ -9,12 +9,12 @@
 
 	<hr>
 
-	<ul id="ta-toc">Loading...</ul>
+	<ol id="ta-toc">Loading...</ol>
 
 	<div class="ta-doc-content">
 		<div class="ta-doc-loading">Fetching documentation... Please wait. <img src="<?php echo admin_url(); ?>/images/spinner-2x.gif" class="ta-doc-loading-img" alt="Loading..."></div>
 	</div>
-	
+
 	<script type="text/javascript" src="<?php echo WPBO_URL; ?>admin/assets/js/vendor/jquery.toc.js"></script>
 	<script type="text/javascript" src="<?php echo WPBO_URL; ?>admin/assets/js/vendor/jquery.smooth-scroll.js"></script>
 	<script type="text/javascript" src="<?php echo WPBO_URL; ?>admin/assets/js/part-documentation.js"></script>
