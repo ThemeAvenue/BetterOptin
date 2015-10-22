0c250aee370e4d81d1d480e6439f46b5d5ff4d0f
Julien Liabeuf <julien@liabeuf.fr>
2015-10-22 17:25:43 +0700

    Move general function

--- includes/providers/aweber/functions-admin.php
+++ includes/providers/aweber/functions-admin.php
@@ -14,43 +14,6 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Check if Aweber settings are correct.
- *
- * @since  1.0.0
- * @return boolean True if Aweber integration is ready to work
- */
-function wpbo_is_aweber_ready() {
-
-	$tokens = maybe_unserialize( get_option( 'wpbo_aweber_tokens' ) );
-
-	if ( ! is_array( $tokens ) ) {
-		return false;
-	}
-
-	$access_token  = isset( $tokens[0] ) ? trim( $tokens[0] ) : '';
-	$access_secret = isset( $tokens[1] ) ? trim( $tokens[1] ) : '';
-
-	if ( empty( $access_token ) || empty( $access_secret ) ) {
-		return false;
-	}
-
-	$auth_code = trim( wpbo_get_option( 'aw_auth_code', '' ) );
-
-	if ( empty( $auth_code ) ) {
-		return false;
-	}
-
-	$list_id = wpbo_get_option( 'aw_list_id', '' );
-
-	if ( empty( $list_id ) ) {
-		return false;
-	}
-
-	return true;
-
-}
-
 add_filter( 'wpbo_checklist', 'wpbo_aw_add_step' );
 /**
  * Add new step.
--- includes/providers/aweber/functions-aweber.php
+++ includes/providers/aweber/functions-aweber.php
@@ -33,3 +33,40 @@ function wpbo_aw_get_lists() {
 	return $aweber->get_lists();
 
 }
+
+/**
+ * Check if Aweber settings are correct.
+ *
+ * @since  1.0.0
+ * @return boolean True if Aweber integration is ready to work
+ */
+function wpbo_is_aweber_ready() {
+
+	$tokens = maybe_unserialize( get_option( 'wpbo_aweber_tokens' ) );
+
+	if ( ! is_array( $tokens ) ) {
+		return false;
+	}
+
+	$access_token  = isset( $tokens[0] ) ? trim( $tokens[0] ) : '';
+	$access_secret = isset( $tokens[1] ) ? trim( $tokens[1] ) : '';
+
+	if ( empty( $access_token ) || empty( $access_secret ) ) {
+		return false;
+	}
+
+	$auth_code = trim( wpbo_get_option( 'aw_auth_code', '' ) );
+
+	if ( empty( $auth_code ) ) {
+		return false;
+	}
+
+	$list_id = wpbo_get_option( 'aw_list_id', '' );
+
+	if ( empty( $list_id ) ) {
+		return false;
+	}
+
+	return true;
+
+}
