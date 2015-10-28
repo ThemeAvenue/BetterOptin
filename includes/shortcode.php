d556f65dcb108deb646a7d045eb9cf5ae8acd020
Julien Liabeuf <julien@liabeuf.fr>
2015-10-28 18:12:14 +0700

    Don't extract shortcode attributes

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -4,34 +4,39 @@ add_shortcode( 'wpbo_popup', 'wpbo_trigger_popup' );
  * Manually triggers a popup.
  *
  * @since  1.0.1
+ *
  * @param  array $atts Shortcode attributes
+ *
  * @return string      HTML link
  */
 function wpbo_trigger_popup( $atts = array() ) {
 
 	$defaults = array(
-		'popup_id'      => Better_Optin::is_popup_available(),
+		'popup_id'      => wpbo_page_has_popup(),
 		'type'          => 'button',
 		'label'         => __( 'Show Popup', 'wpbo' ),
 		'bypass_cookie' => true,
 		'btn_class'     => ''
 	);
 
-	extract( shortcode_atts( $defaults, $atts ) );
+	$atts     = shortcode_atts( $defaults, $atts );
+	$popup_id = (int) $atts['popup_id'];
 
 	/* No popup ID? Bye bye... */
-	if( false === $popup_id )
+	if ( false === $atts['popup_id'] ) {
 		return false;
+	}
 
 	/* Do NOT bypass the cookie */
-	if( isset( $_COOKIE["wpbo_$popup_id"] ) && false === boolval( $bypass_cookie ) )
+	if ( isset( $_COOKIE["wpbo_$popup_id"] ) && false === (bool) $atts['bypass_cookie'] ) {
 		return false;
+	}
 
-	if( 'button' == $type )
-		$sc = "<button class='wpbo-trigger $btn_class'>$label</button>";
-
-	else
-		$sc = "<a href='#' class='wpbo-trigger $btn_class'>$label</a>";		
+	if ( 'button' == $atts['type'] ) {
+		$sc = "<button class='wpbo-trigger {$atts['btn_class']}'>{$atts['label']}</button>";
+	} else {
+		$sc = "<a href='#' class='wpbo-trigger {$atts['btn_class']}'>{$atts['label']}</a>";
+	}
 
 	return $sc;
 
